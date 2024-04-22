e4cbbed57427bab5d1c25a389ef70fce3039dbc4
Bogdan Oprisiu <bobo.oprisiu@gmail.com>
2024-04-22 11:10:36 +0300

    .

--- DBMS_Project/computer-store-be-python/authentication/schemas.py
+++ DBMS_Project/computer-store-be-python/authentication/schemas.py
@@ -11,28 +11,3 @@ class UserCreate(BaseModel):
 class UserAuthenticate(BaseModel):
     email: str
     password: str
-
-
-class OperatingSystem(BaseModel):
-    os_name: str
-    os_version: int
-    os_version_name: str
-
-
-class ComputerManufacturer(BaseModel):
-    manufacturer_name: str
-    manufacturer_email: str
-    manufacturer_phone: int
-
-
-class ComputerCategory(BaseModel):
-    category_name: str
-    category_description: str
-
-
-class Computer(BaseModel):
-    model_name: str
-    base_price: int
-    os_id: int
-    manufacturer_id: int
-    category_id: int
